6bd5300b5cfbc77a0330a3a1e1c711b0d3ad0b5e
jithin R <jeets2689@gmail.com>
2017-06-23 17:05:27 +0530

    added associations between users and articles and debbuger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
  @articles=Article.all
  end
  def new
+
  	@article = Article.new
 
  end	
@@ -14,7 +15,10 @@ end
 
  def create
  #render plain:params[:article].inspect
+  debugger
  @article = Article.new(article_params)
+@article.user = User.first
+
 if @article.save
    flash[:success]="Article was successfully created"
  redirect_to article_path(@article)
