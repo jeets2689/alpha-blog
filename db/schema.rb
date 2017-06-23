e448372b6ed93a33ede1d22fab48ebd7d38af026
jithin R <jeets2689@gmail.com>
2017-06-23 12:12:47 +0530

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616065539) do
+ActiveRecord::Schema.define(version: 20170616063018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "title"
   end
 
 end
