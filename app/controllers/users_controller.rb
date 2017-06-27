d2f8f3a6c02e8cb411cfeec6ab5f5c3992fae2a0
jithin R <jeets2689@gmail.com>
2017-06-27 15:57:20 +0530

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+before_action :set_user, only: [:edit, :update, :show]
+before_action :require_same_user, only: [:edit, :update]
+
 def index
 @users = User.paginate(page: params[:page], per_page: 5)
 end
@@ -25,11 +28,9 @@ end
 end
 
 def edit 
-@user = User.find(params[:id])
 end
 
 def update
-@user =User.find(params[:id])
 if @user.update(user_params)
   flash[:success]="Article was successfully updated"
    redirect_to articles_path
@@ -42,7 +43,7 @@ end
 end
 
 def show
- @user = User.find(params[:id])
+
  @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
 end
@@ -52,5 +53,14 @@ private
 def user_params
   params.require(:user).permit(:username, :email, :password)
 end
+def set_user
+ @user = User.find(params[:id])
+end
 
+def require_same_user
+  current_user != @user
+ 	flash[:danger] = "You can only edit your account"
+    redirect_to root_path
+  
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ root 'pages#home'
  get 'signup', to: 'users#new'
  resources :users, expect: [:new]
 
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
+
 end
