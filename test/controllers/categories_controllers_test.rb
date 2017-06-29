fdcfeafda9d46dd0239d9eeeada4dd0c773d7991
jithin R <jeets2689@gmail.com>
2017-06-29 20:40:57 +0530

    add admin requirement to create categories

--- test/controllers/categories_controllers_test.rb
+++ test/controllers/categories_controllers_test.rb
@@ -5,7 +5,7 @@ class CategoriesControllerTest < ActionController::TestCase
 def setup
 
 @category = Category.create(name: "sports")
-
+@user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
 end
 
 test "should get categories index" do
@@ -17,7 +17,7 @@ assert_response :success
 end
 
 test "should get new" do
-
+session[:user_id] = @user.id
 get :new
 
 assert_response :success
