f3693920917bb943a80c36122c7eb78311524fb6
jithin R <jeets2689@gmail.com>
2017-06-29 23:00:17 +0530

    Add articles and category associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
-  
+  has_many :article_categories
+
+   has_many :articles, through: :article_categories
   validates :name, presence: true
   validates :name, length: 3..25
   
